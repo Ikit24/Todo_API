3e7f80ec8a6c690f22806818bd3ba4ae8f533ee5
AttilaSz <szszattila2@gmail.com>
2025-03-13 17:58:17 +0200

    started implementing user auth

--- app/main.py
+++ app/main.py
@@ -4,15 +4,40 @@
 # Get a list of tasks, filter them by status and get the details of each one.
 # fastapi dev main.py - to start the server
 
+from typing import Annotated
+
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+from .user_auth import *
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class Article(BaseModel):
     id: int
     name: str
     price: float
 
-app = FastAPI()
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 articles = {}
 
